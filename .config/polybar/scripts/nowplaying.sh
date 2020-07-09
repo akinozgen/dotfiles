13081802dec007c2d98329725a7beb5cc939f362
Akın Özgen <akinozgen17@outlook.com>
2020-07-09 16:05:52 +0300

    tweaks

--- .config/polybar/scripts/nowplaying.sh
+++ .config/polybar/scripts/nowplaying.sh
@@ -3,10 +3,10 @@
 re='^[0-9]+$'
 
 while true; do
+  clear
 	exists="$(pgrep deadbeef)"
 	if [[ $exists =~ $re ]] ; then
 		output="$(deadbeef --nowplaying-tf '%artist% - %title%')"
-		clear
 		echo $output
 	else
 		echo 'Not playing'
