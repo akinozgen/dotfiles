ac1307ae136cdaac28f2eb73a7ea3da21c73871d
Akın Özgen <akinozgen17@outlook.com>
2020-12-11 15:13:37 +0300

    last files function gives better results. mount ntfs function added

--- .local/functions.sh
+++ .local/functions.sh
@@ -8,18 +8,20 @@ add_alias() {
 	source ~/.local/aliases.sh
 }
 
-togb() {
-  echo $(echo $1 | awk '{print /1000/1000/1000 " GB "}')
-}
 
 lastxfiles() {
-  find . -type f -printf '%T@ %p\n' | sort -n | tail -$1 | awk '{print $2, $3, $4, $5, $6, $7, $8}' | tac
+find . -type f -printf '%T@ %p\n' | sort -n | tail -$1 | awk '{print $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15}' | tac
 }
-
 ytvideo() {
 	youtube-dl --output "%(uploader)s|%(title)s.%(ext)s" "$1"
 }
 
 ytmp3() {
 	youtube-dl --extract-audio --audio-format mp3 --output "%(title)s.%(ext)s" "$1"
+}
+
+
+mount_ntfs() {
+  sudo mkdir -p $2
+  sudo /usr/local/bin/ntfs-3g $1 $2 -olocal -oallow_other -oremove_hiberfile
 }
