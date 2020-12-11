1c7476c24ad87fd3f45c1c866c48ada2a6aa008e
Akın Özgen <akinozgen17@outlook.com>
2020-12-11 15:09:51 +0300

    paths revamped

--- .local/exports.sh
+++ .local/exports.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-export XDG_CONFIG_HOME="$HOME/.config/"
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"
-export ANDROID_HOME="/home/akinozgen/.config/AndroidSdk/Sdk"
-export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/home/akinozgen/Android/Sdk/build-tools/29.0.1:$JAVA_HOME/bin:$ANDROID_HOME/build-tools/29.0.2/:$HOME/.local/bin/:$HOME/.config/AndroidSdk/flutter/bin:$HOME/.config/composer/vendor/bin"
-export NVM_DIR="$HOME/.nvm"
-export ZSH="/home/akinozgen/.oh-my-zsh"
-export ZSH_THEME="akinozgen"
+export JAVA_HOME="/usr/lib64/jvm/java-8-openjdk/"
+export ANDROID_HOME="/home/akinozgen/Android/Sdk"
+export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/30.0.2/:$JAVA_HOME/bin:$HOME/.local/bin/:$HOME/.config/flutter/bin:$HOME/.cargo/bin"
 
-export ANDROID_SDK_HOME="$XDG_CONFIG_HOME/AndroidSdk"
-export ANDROID_AVD_HOME="$XDG_DATA_HOME/android/"
-export ANDROID_EMULATOR_HOME="$XDG_DATA_HOME/android"
-export ADB_VENDOR_KEY="$XDG_CONFIG_HOME/android"
+export ANDROID_SDK_HOME="$ANDROID_HOME"
+export ANDROID_AVD_HOME="$HOME/.android/"
+export ANDROID_EMULATOR_HOME="$HOME/.android"
+export ADB_VENDOR_KEY="$HOME/.android"
 
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 # wal -i $(cat .config/nitrogen/bg-saved.cfg | grep 'file=' | sed 's/file\=//g')
+export VIRMACH=107.172.43.156
+export ANDROID_SDK_ROOT=$ANDROID_HOME
